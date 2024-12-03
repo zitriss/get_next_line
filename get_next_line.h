38b125f5862cb2edfd7c10ab05eaffcc3202154c
Tristan Lize <tlize@c2r6p4.42lehavre.fr>
2024-12-03 16:46:16 +0100

    minor changes

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,16 @@
 /*   By: tlize <tlize@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 16:51:02 by tlize             #+#    #+#             */
-/*   Updated: 2024/11/27 17:11:43 by tlize            ###   ########.fr       */
+/*   Updated: 2024/12/03 15:27:29 by tlize            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_MEXT_LINE_H
-# define GET_MEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdlib.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-char    *get_next_line(int fd);
+char	*get_next_line(int fd);
 
 #endif
